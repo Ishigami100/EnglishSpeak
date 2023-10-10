0d9893ade6e47ee22c518c834fc9bdac6a01eb14
Ishigami100 <yamahaya5151@icloud.com>
2023-10-11 02:44:44 +0900

    manual update

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'csv'
 
-#CSV.foreach('db/ejdict.csv',quote_char: "\x00", force_quotes: true) do |row|
-#    p row[0];
-#    p row[1];
-#    Word.create(:word => row[0], :mean => row[1])
-#end
+CSV.foreach('db/ejdict.csv',quote_char: "\x00", force_quotes: true) do |row|
+    p row[0];
+    p row[1];
+    Word.create(:word => row[0], :mean => row[1])
+end
 
-#User.create(username:"aaa",password:"password",times:0)
+User.create(username:"aaa",password:"password",times:0)
 
-#ConversationHistory.create(userid:1,context:"Good Bye!",conversation_times:1,gpt_flag:false)
+ConversationHistory.create(userid:1,context:"Good Bye!",conversation_times:1,gpt_flag:false)
 
 UserWord.create(word_number:1,userid:1,count:1)
