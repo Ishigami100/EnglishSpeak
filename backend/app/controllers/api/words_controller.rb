4fec52bc6f86668fb1013f47e8d6d370aef6f013
Ishigami100 <yamahaya5151@icloud.com>
2023-10-12 01:21:41 +0900

    update /words_controller.rb

--- backend/app/controllers/api/words_controller.rb
+++ backend/app/controllers/api/words_controller.rb
@@ -1,15 +1,19 @@
 require 'faraday'
 class Api::WordsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def create
-        if word = Word.find_by(word:word_params)
-            return
+        word = Word.find_by(word: params["word"])
+        if word
+            return # 該当するレコードが既に存在する場合
+        end
+        data = translate(word:params["word"])
+        if data.present?
+            p data
+            Word.create(word: params["word"], mean: data)
+            p "aaa"
         else
-            data = translate(word_params)
-            if data == ''
-                return
-            else
-                Word.create(word:word_params, mean:data)
-            end
+            # dataが空の場合のエラーハンドリングを追加
+            p "エラー: データの翻訳に失敗しました"
         end
     end
 
@@ -18,25 +22,30 @@ class Api::WordsController < ApplicationController
         render json: word
     end
 
-    def word_params
-        params.permit(:word)
-    end
     def translate(word)
-        conn = Faraday.new(url: 'https://script.google.com/macros/s/AKfycbydfqOuOeylPG1z_6YGGp97fFHUYuLRNhYiV-3-G7p5L31InF4v3IDx7AY-vBq1yLVp9Q/exec')
-        response = conn.get do |req|
-          req.params['text'] = word
-          req.params['source'] = 'en'
-          req.params['target'] = 'jp'
-        end
-        
-    
-        if response.success?
-          # 成功時の処理
-          data = JSON.parse(response.body)
-          mean = data['text']
+        original_url='https://script.google.com/macros/s/AKfycby4Eoh1XfplqHCTMhLuym2Zbv6hFACqN543-pHnEzAtPlt4olThqEF3-2yfWpdVQ6Ko1Q/exec'
+        # クエリパラメータを含めたURLを生成
+        url_with_query = "#{original_url}?text=#{word}&source=en&target=ja"
+
+        conn = Faraday.new(url:url_with_query)
+        p response = conn.get
+
+        if response.status == 302
+            # リダイレクト先URLを取得
+            redirect_url = response.headers['Location']
+            # リダイレクト先URLに新しいリクエストを送信
+            conn = Faraday.new
+            response = conn.get(redirect_url)
+            if response.success?
+                # リダイレクト先URLからデータを取得
+                p mean = JSON.parse(response.body)
+                data = mean['text']
+            else
+                # 失敗時の処理
+                data = ''
+            end
         else
-          # 失敗時の処理
-          data=''
+            data=''
         end
-    end 
+    end
 end
